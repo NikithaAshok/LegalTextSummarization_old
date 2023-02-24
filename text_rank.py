ee124a7576af2f2bad5105452d546a131cca7fb5
krithikajain <124558745+krithikajain@users.noreply.github.com>
2023-02-25 01:18:56 +0530

    Update added Lex Rank

--- text_rank.py
+++ text_rank.py
@@ -15,6 +15,7 @@ from nltk.stem import PorterStemmer
 import re
 import string
 from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.parsers.plaintext import PlaintextParser
 
@@ -34,8 +35,6 @@ def remove_stop_words(text):
     punct_removed_text = text.translate(str.maketrans('','',string.punctuation))
     words = nltk.word_tokenize(punct_removed_text)
     
-    #stop_words = set(stop_words.words('english'))
-    
     words = [word for word in words if word.lower() not in stop_words]
     return " ".join(words)
 
@@ -48,8 +47,6 @@ def stemming(text):
 
     #stem the tokens 
     stemmed_tokens = []
-
-
     for token in tokens:
         stemmed_token = stemmer.stem(token)
         stemmed_tokens.append(stemmed_token)
@@ -60,19 +57,26 @@ def stemming(text):
     return stemmed_text
 
 
-
+extracted_text=' ' #initialise for multiple pages
 for i in range(0,len(pdf_reader.pages)):
     page = pdf_reader.pages[i]
-    extracted_text = page.extract_text() # extraction happens here
+    extracted_text += page.extract_text() # extraction happens here
     text_without_stopwords = remove_stop_words(extracted_text) #removing stopwords (function called)
     #stemmed_tokens = [stemmer.stem(word) for word in text_without_stopwords]
     sentences = sent_tokenize(extracted_text)
     #sentences = [sentence for sentence in sentences if not any(word.lower() in stop_words for word in sentence.split())]
     
-    parser = PlaintextParser.from_string('\n'.join(sentences),Tokenizer('english'))
-    summarizer = TextRankSummarizer()
-    summary = summarizer(parser.document,4) 
-    #summarizer has arguments document and sentences_count
-    print("Summary :",summary)
+    # text rank --- TextRank looks at the semantic relationships between sentences.
+    txt_parser = PlaintextParser.from_string('\n'.join(sentences),Tokenizer('english'))
+    txt_rank_summarizer = TextRankSummarizer()
+    txt_summary = txt_rank_summarizer(txt_parser.document,4) #summarizer has arguments (document,sentences_count)
+    print("Text Rank Summary :",txt_summary)
+
+    # lex rank --- LexRank looks at the lexical overlap between sentences.
+    lex_parser = PlaintextParser.from_string(extracted_text,Tokenizer('english'))
+    lex_summarizer = LexRankSummarizer()
+    lex_summary = lex_summarizer(lex_parser.document, sentences_count=3)
+    print("Lex Rank Summary: ",lex_summary)     
+
     
 
